cbaaa97f1ce01ede5e6e5ed99b5bf91c9d747eec
parshwa26 <40566116+parshwa26@users.noreply.github.com>
2020-09-13 14:30:19 -0400

    Update AppConstants.java

--- AgroExpert/app/src/main/java/com/agroexpert/expert/Utils/AppConstants.java
+++ AgroExpert/app/src/main/java/com/agroexpert/expert/Utils/AppConstants.java
@@ -11,66 +11,6 @@ public class AppConstants {
     public  static SharedPreferences preferences;
 
 
-    //public static final String URL = "http://architectmihirthaker.com/architectmihirthaker.com/gh01/";
-    //public static final String URL = "http://pshah.in/";
-    //public static final String URL = "http://online.jau.in:2015/agriex/common/";
-    //public static final String URL1 = "http://online.jau.in:2015/agriex/expert/";
-    //public static final String URL2 = "http://online.jau.in:2015/agriex/";
-
-    public static final String URL = "http://pshah.in/agriex/common/";
-    public static final String URL1 = "http://pshah.in/agriex/expert/";
-    public static final String URL2 = "http://pshah.in/agriex/";
-
-    public static final String key = "4336f0f199bd35b3275e7b77e6471da3985f963b";
-    public static final String exlogin = URL1 + "exlogin.php";
-    public static final String insertquestion = URL + "insertquestion.php";
-    public static final String insertquestion2 = URL + "insertquestion2.php";
-    public static final String Registration = URL + "Registration.php";
-    public static final String logindetails = URL + "logindetails.php";
-    public static final String responselid = URL + "responselid.php";
-    public static final String selectallquestion = URL + "selectallquestion.php";
-    public static final String selectquestion = URL + "selectquestion.php";
-    public static final String updatedistrict = URL + "updatedistrict.php";
-    public static final String otpverify = URL + "otpverify.php";
-    public static final String category = URL + "category.php";
-    public static final String subcategory = URL + "subcategory.php";
-    public static final String answeredquestion = URL + "answeredquestion.php";
-    public static final String unansweredquestion = URL1 + "exunansweredquestion.php";
-    public static final String faqs = URL + "faqs.php";
-    public static final String searchquestion = URL + "searchquestion.php";
-    public static final String starredlist = URL + "starredlist.php";
-    public static final String addlike = URL + "addlike.php";
-    public static final String totallike = URL + "totallike.php";
-    public static final String addtostar = URL + "addtostar.php";
-    public static final String detailstarredlist = URL + "detailstarredlist.php";
-    public static final String detailsearchquestion = URL + "detailsearchquestion.php";
-    public static final String instruction = URL + "instruction.php";
-    public static final String detailinstruction = URL + "detailinstruction.php";
-
-
-
-
-
-    public static final String exunansweredquestion = URL1 + "exunansweredquestion.php";
-    public static final String exanswer = URL1+ "exanswer.php";
-    public static final String exansweredquestion = URL1 + "exansweredquestion.php";
-    public static final String district = URL + "district.php";
-    public static final String profiledetail = URL1 + "exprofiledetails.php";
-    public static final String exdetailansweredquestion = URL1 + "exdetailansweredquestion.php";
-    public static final String exdetailunansweredquestion = URL1 + "exdetailunansweredquestion.php";
-    public static final String exupdatcid = URL1 + "exupdatecid.php";
-
-
-    public static final String uploadvideo = URL + "upload.php";
-    public static final String uploadaudio = URL + "uploadaudio.php";
-    public static final String uploadimage = URL + "uploadimage.php";
-
-
-    public static final String fetchimageurl = URL2 + "images/";
-    public static final String fetchvideourl = URL2 + "video/";
-    public static final String fetchaudiourl = URL2 + "audio/";
-
-
-
+    //AVAILABLE ON CONTACTING AUTHOR
 
 }
